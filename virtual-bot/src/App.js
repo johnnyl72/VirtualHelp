984e7e4b1b9354421555ca8d64e46e4168ea4193
jomamma417 <johnnyl@cpp.edu>
2020-03-11 23:35:36 -0700

    Initialize repo

---
+++ virtual-bot/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Home"
+class App extends React.Component {
+  render(){
+    return (
+       < Home/>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src="https://cdn.britannica.com/s:700x500/67/183767-050-23D72392/World-Data-Locator-Map-Sweden.jpg" />
+      //     <h1>Under Construction. We'll be back soon ;)</h1>
+      //
+      //   </header>
+      // </div>
+    );
+  }
+}
+
+export default App;
