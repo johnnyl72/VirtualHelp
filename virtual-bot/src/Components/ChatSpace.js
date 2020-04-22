7c2689b8d5eb36c334f91a85fe489abe187aa8b5
jomamma417 <johnnyl@cpp.edu>
2020-04-21 23:15:10 -0700

    integrate dialogflow

--- virtual-bot/src/Components/ChatSpace.js
+++ virtual-bot/src/Components/ChatSpace.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {sendMessage} from './chatAPI';
 
-function ChatSpace(){
-  return(
-    <div class="centerHeader pt-5 mt-5">
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-      <h1>APPLES AND BEES</h1>
-    </div>
-
-  )
+class ChatSpace extends Component {
+  render(){
+    const {feed, sendMessage} = this.props;
+    return (
+      <div class="centerHeader mt-5 pt-5">
+        <h1>Welcome there!</h1>
+        <u1>
+          {feed.map(entry => <li>{entry.text}</li>)}
+        </u1>
+        <input type="text" onKeyDown={ e=> e.keyCode === 13 ? sendMessage(e.target.value) : null }/>
+      </div>
+    );
+  }
 }
 
-export default ChatSpace
+const mapStateToProps = state => ({
+  feed: state
+});
+
+export default connect(mapStateToProps, {
+  sendMessage
+})(ChatSpace);
--- virtual-bot/src/Components/chatAPI.js
+++ virtual-bot/src/Components/chatAPI.js
@@ -1,36 +1,42 @@
-import {ApiAIClient} from 'api-ai-javascript';
-import {createStore, applyMiddleware} from 'redux';
+import {ApiAiClient} from 'api-ai-javascript'
+import {applyMiddleware, createStore} from 'redux'
 
-let token = '89323893eb0c4243ab337d6aa2f09ea7'
-const client = new ApiAIClient({token})
-const MSG_RECIEVE = 'MSG_RECIEVE';
+const accessToken = '89323893eb0c4243ab337d6aa2f09ea7'
+const client = new ApiAiClient({accessToken})
+const ON_MESSAGE = 'ON_MESSAGE'; //generic event
 
-export const sendMsg = (text, sender='user') => ({
-  type: MSG_RECIEVE,
+export const sendMessage = (text, sender='user') => ({
+  type: ON_MESSAGE,
   payload: {text, sender}
 });
 
-const msgMiddleWare = () => next => action => {
+const messageMiddleware = () => next => action => {
   next(action);
 
-  if(action.type == MSG_RECIEVE){
-    const {text} = action.payload;
-    client.textRequest(text).then(onSuccess)
+  if(action.type === ON_MESSAGE){
+    const { text } = action.payload;
+    client.textRequest(text)
+    .then(onSuccess)
 
     function onSuccess(response){
-      const {result: {fulfillment}} =response;
-      next(sendMessage(fullfillment.sppech, 'bot'))
+      const {result: {fulfillment}} = response;
+      next(sendMessage(fulfillment.speech, 'bot'));
     }
   }
 };
-const initState = [{text: 'Hello'}];
-const msgReducer=(state=initState,action)=>{
-    switch(action.type){
-        case MSG_RECIEVE:
-            return  [...state,action.payload]
-            default:
-                return state;
-    }
-}
 
-export const store = createStore(msgReducer,msgMiddleWare)
+const initState = [{text: 'Hey, I need help!'}];
+//Reducer
+const messageReducer = (state = initState, action) => {
+  switch(action.type){
+
+    case ON_MESSAGE:
+      return [...state, action.payload];
+
+    default:
+      return state;
+
+  }
+};
+
+export const store = createStore(messageReducer, applyMiddleware(messageMiddleware))
