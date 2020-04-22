0f28cfcffc65fa6e16709ada2c66640933e5d897
jomamma417 <johnnyl@cpp.edu>
2020-04-22 00:58:19 -0700

    fix styles for mobile

--- virtual-bot/src/Components/Home.js
+++ virtual-bot/src/Components/Home.js
@@ -9,7 +9,7 @@ function Home(){
           <div style={{backgroundImage: "url(https://wallpaperplay.com/walls/full/8/4/2/86369.jpg)", filter: "contrast(200%)"}}>
             <div style={{paddingTop: "10%", paddingBottom: "10%"}}>
               <div className="row center">
-                <div className="col-lg-3 fadeIn">
+                <div className="col-lg-3 fadeIn img-fluid mb-3 d-none d-lg-block">
                   <img src="https://cdn.clipart.email/1468a43d9aab498728e693cefdc57f7f_28-collection-of-robot-clipart-transparent-high-quality-free-_2220-2000.png"  style={{width: "330px", position: "relative", right: "10rem",filter: "hue-rotate(333deg)"}}/>
                 </div>
                 <div className="col-lg-6 fadeIn">
@@ -18,10 +18,10 @@ function Home(){
                   <p class="subheading" style={{color:"white"}}>Sometimes we get all tangled up inside our heads, unable to move on. Help Bot is great at helping you get unstuck. Co-designed by therapists, coaches, users and AI folk, Help Bot lets you set the pace, helps when it can, and never judges. It is free and anonymous - so give it a try!</p>
                   <p class="subheading" style={{color:"white"}}>Nothing can match the privacy of an anonymous conversation with an AI bot. Think of it as an interactive journal meets life coach. Wysa is good at asking the right probing questions, and helping you untangle and unwind after a hard day.</p>
                 </div>
-                <div className="col-lg-3 fadeIn">
+                <div className="col-lg-3 fadeIn d-none d-lg-block">
                   <img src="https://woebot.io/static/app-d427756f065bfa07d4125d9be3d8ee8d.gif" alt="" style={{width: "95%", position: "relative", left: "30%", translateX: "25%"}} className="Phone"/>
                 </div>
-                <div className="col-lg-12 fadeInLeft" style={{position: "relative", right: "15%"}}>
+                <div className="col-lg-12 fadeInLeft d-none d-lg-block" style={{position: "relative", right: "15%"}}>
                   <img src="https://pluspng.com/img-png/get-it-on-google-play-png-file-get-it-on-google-play-svg-1280.png" alt="" style={{width:"20rem", padding: "15px" }}/>
                   <img src="https://cdn.worldvectorlogo.com/logos/download-on-the-app-store-apple.svg" alt="" style={{width:"20rem", padding: "15px"}}/>
                 </div>
@@ -37,7 +37,7 @@ function Home(){
             {/* </ol> */}
             <div className="carousel-inner">
               <div className="carousel-item active">
-                <img className="cover me" src="https://images.pexels.com/photos/3787818/pexels-photo-3787818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
+                <img className="cover me " src="https://images.pexels.com/photos/3787818/pexels-photo-3787818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
                 <div className="container">
                   <div className="carousel-caption text-left">
                     <h1>Example headline.</h1>
@@ -79,26 +79,26 @@ function Home(){
 
           <div className="center" style={{width: "90%"}}>
 
-            <div className="testimonals">
+            <div className="testimonals mb-5">
                 <div>
                   <h2 className="featurette-heading" style={{color: "white"}}>idk what to put here</h2>
                   <p className="lead" style={{color:"white"}}>Through trial and error, I found a wonderful psychologist, who was able to help me through that time by using talk-therapy. Later on, I realized that when I was speaking with my friends and colleagues, I was simply repeating the conversations I previously had with my psychologist.
                     That’s when I first realized: "If I can help people by repeating these conversations, then we could teach a machine to do the same."</p>
                 </div>
-                <img className="meBox" src="https://assets.website-files.com/5b514e705d6e7eb2ee91092e/5b516121584bd75a2c0d7b89_Michiel_Headshot-p-500.jpeg" alt="" style={{width: "10%"}}/>
+                <img className="meBox d-none d-lg-block" src="https://assets.website-files.com/5b514e705d6e7eb2ee91092e/5b516121584bd75a2c0d7b89_Michiel_Headshot-p-500.jpeg" alt="" style={{width: "10%"}}/>
             </div>
 
-            <div className="testimonals">
+            <div className="testimonals mb-5">
               <div>
                 <h2 className="featurette-heading" style={{color: "white"}}>idk what to put here</h2>
                   <p className="lead" style={{color:"white"}}>Through trial and error, I found a wonderful psychologist, who was able to help me through that time by using talk-therapy. Later on, I realized that when I was speaking with my friends and colleagues, I was simply repeating the conversations I previously had with my psychologist.
                     That’s when I first realized: "If I can help people by repeating these conversations, then we could teach a machine to do the same."</p>
                 </div>
-                <img className="meBox" src="https://assets.website-files.com/5b514e705d6e7eb2ee91092e/5b516121584bd75a2c0d7b89_Michiel_Headshot-p-500.jpeg" alt="" style={{width: "10%"}}/>
+                <img className="meBox d-none d-lg-block" src="https://assets.website-files.com/5b514e705d6e7eb2ee91092e/5b516121584bd75a2c0d7b89_Michiel_Headshot-p-500.jpeg" alt="" style={{width: "10%"}}/>
             </div>
 
-            <div className="testimonals">
-              <img className="meBox" src="https://assets.website-files.com/5b514e705d6e7eb2ee91092e/5b516121584bd75a2c0d7b89_Michiel_Headshot-p-500.jpeg" alt="" style={{width: "10%"}}/>
+            <div className="testimonals mb-5">
+              <img className="meBox d-none d-lg-block" src="https://assets.website-files.com/5b514e705d6e7eb2ee91092e/5b516121584bd75a2c0d7b89_Michiel_Headshot-p-500.jpeg" alt="" style={{width: "10%"}}/>
               <div>
                 <h2 className="featurette-heading" style={{color: "white"}}>idk what to put here</h2>
                 <p className="lead" style={{color:"white"}}>Through trial and error, I found a wonderful psychologist, who was able to help me through that time by using talk-therapy. Later on, I realized that when I was speaking with my friends and colleagues, I was simply repeating the conversations I previously had with my psychologist.
--- virtual-bot/src/Components/HowItWorks.js
+++ virtual-bot/src/Components/HowItWorks.js
@@ -5,53 +5,35 @@ function HowItWorks(){
 
     <div className="py-5 container">
       <h1 className= "centerHeader" style={{paddingTop: "10%", paddingBottom: "10%"}}>The science behind Help Bot</h1>
-      <div className= "py-5 flex-about">
-        <div className="col-6">
-          <h1>Who do you think you are?</h1>
-        </div>
-        <div className="col-6">
-          <span>We are psychologists who built programs and tools in the clinic. We worked at Stanford for over 10 years.
-            We love helping people & behavioral science (so much so we’ve been doing it for 20 years).</span>
-        </div>
+
+      <div className="row pb-5 m-2">
+        <h1 className="col-lg-6 center">
+          Who do you think you are?
+        </h1>
+        <span className="float-right col-lg-6">We are psychologists who built programs and tools in the clinic. We worked at Stanford for over 10 years.
+          We love helping people & behavioral science (so much so we’ve been doing it for 20 years).</span>
       </div>
-      <div className= "pt-5 flex-about">
-        <div className="col-6">
-          <h1>So why leave the clinic?</h1>
-        </div>
-        <div className="col-6">
-          <span>Because the sad truth is that more than half of the world’s population still doesn’t have access to basic health care.
-            For many more, mental health care is not an option.So we asked ourselves, “what tools can we use to help?”</span>
-        </div>
+
+      <div className="row pb-5 m-2">
+        <h1 className="col-lg-6 center">
+          So why leave the clinic?
+        </h1>
+        <span className="float-right col-lg-6">Because the sad truth is that more than half of the world’s population still doesn’t have access to basic health care.
+          For many more, mental health care is not an option.So we asked ourselves, “what tools can we use to help?”</span>
       </div>
-      <div className= "pt-5 flex-about">
-        <div className="col-6">
-          <h1>Don’t you need a therapist?</h1>
-        </div>
-        <div className="col-6">
-          <span>Not always. There’s 20+ years of rigorous research to show that DIY CBT works. CBT delivered via the Internet (and even video games!)
-             can be as effective as therapist-delivered CBT for both anxiety and depression.Most studies show that people do better with some coaching.
-             Coaching is not the same as therapy: coaches redirect to relevant material but people still need to do the work themselves.</span>
-        </div>
-      </div>
-      <div className= "pt-5 flex-about">
-        <div className="col-6">
-          <h1>So what did you do?</h1>
-        </div>
-        <div className="col-6">
-          <span>
-            We brought in storytellers, data scientists, engineers, and designers to combine their love of writing stories, AI, and robots with the
-            intention of creating a companion (Woebot!) who can help people with their mental health.But before launching Woebot publicly, we wanted
-             to make sure he could truly help people so we did a randomized controlled trial…</span>
-        </div>
-      </div>
-      <div>
-        <footer className="container">
-          <hr className="featurette-divider" />
-          <p className="float-right"><a href="#">Back to top</a></p>
-          <p>© 2017-2019 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-        </footer>
+
+      <div className="row pb-5 m-2">
+        <h1 className="col-lg-6 center">
+          Don’t you need a therapist?
+        </h1>
+        <span className="float-right col-lg-6">Not always. There’s 20+ years of rigorous research to show that DIY CBT works. CBT delivered via the Internet (and even video games!)
+           can be as effective as therapist-delivered CBT for both anxiety and depression.Most studies show that people do better with some coaching.
+           Coaching is not the same as therapy: coaches redirect to relevant material but people still need to do the work themselves.</span>
       </div>
     </div>
+
+
+
   )
 }
 
